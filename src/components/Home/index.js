ff96a8fddd7e64c22ab7478023db94867c51dfbc
Marcus Warnsley <marcus@Marcuss-MacBook-Air.local>
2019-02-11 23:00:18 -0500

    Adding in the fetching of the items from the home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,8 +8,54 @@ import Spinner from '../elements/Spinner';
 
 import './Home.css';
 
+const endpoint = `${process.env.REACT_APP_API_URL}movie/popular?api_key=${
+    process.env.REACT_APP_API_KEY
+}&language=en-US&page=1`;
+
 class Home extends Component {
-    state = {};
+    state = {
+        currentPage: 0,
+        heroImage: null,
+        isLoading: false,
+        movies: [],
+        searchTerm: '',
+        totalPages: 0
+    };
+    componentDidMount = () => {
+        this.setState({ isLoading: true });
+        this.fetchItems(endpoint);
+    };
+    loadMoreItems = () => {
+        const { currentPage, searchTerm } = this.state;
+        let endpoint = '';
+
+        this.setState({ isLoading: true });
+
+        if (searchTerm === '') {
+            endpoint = `${process.env.REACT_APP_API_URL}movie/popular?api_key=${
+                process.env.REACT_APP_API_KEY
+            }&language=en-US&page=${currentPage + 1}`;
+        } else {
+            endpoint = `${process.env.REACT_APP_API_URL}search/movi?api_key=${
+                process.env.REACT_APP_API_KEY
+            }&language=en-US&query${searchTerm}&page=${currentPage + 1}`;
+        }
+        this.fetchItems(endpoint);
+    };
+    fetchItems = endpoint => {
+        fetch(endpoint)
+            .then(response => response.json())
+            .then(res => {
+                this.setState({
+                    currentPage: res.page,
+                    heroImage: this.state.heroImage || res.results[0],
+                    isLoading: false,
+                    movies: [...this.state.movies, ...res.results],
+                    totalPages: res.totalPages
+                });
+            })
+            .catch(error => console.error(error));
+    };
     render() {
         return (
             <div className="rmdb-home">
